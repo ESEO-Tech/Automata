13a1f4bfffcbb2366331e70df79a1413a506f98e
senshu <guillaume.savaton@gmail.com>
2013-11-05 11:11:10 +0100

    Robot : detect collisions

--- games/robot/World.js
+++ games/robot/World.js
@@ -61,8 +61,34 @@ namespace(this, "automata.games.robot", function (exports, globals) {
             }
 
             // Detect collisions
+            function intersect(cx, cy, r, wx, wy1, wy2) {
+                var r2 = r * r;
+                var d = wx - cx;
+                var d2 = d * d;
+                if (r2 < d2) {
+                    return false;
+                }
+                else {
+                    var s = Math.sqrt(r2 - d2);
+                    var y1 = cy + s;
+                    var y2 = cy - s;
+                    return y1 >= wy1 && y1 <= wy2 || y2 >= wy1 && y2 <= wy2;
+                }
+            }
+            
             var collision = false;
-            // TODO
+            
+            for (var index = 0; index < this.walls.length; index ++) {
+                var w = this.walls[index];
+                if (intersect(nextMatrix.e, nextMatrix.f, this.robotRadius, w[0], w[1], w[3]) ||
+                    intersect(nextMatrix.e, nextMatrix.f, this.robotRadius, w[2], w[1], w[3]) ||
+                    intersect(nextMatrix.f, nextMatrix.e, this.robotRadius, w[1], w[0], w[2]) ||
+                    intersect(nextMatrix.f, nextMatrix.e, this.robotRadius, w[3], w[0], w[2])) {
+                    collision = true;
+                    break;
+                }
+            }
+            
             if (!collision) {
                 this.robotMatrix = nextMatrix;
             }
