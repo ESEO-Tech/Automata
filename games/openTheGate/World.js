5550981c8dcfa655a6adf8995d1144d07c9188b0
senshu <senshuapash@gmail.com>
2013-12-08 10:06:44 +0100

    Manage two cars

--- games/openTheGate/World.js
+++ games/openTheGate/World.js
@@ -38,53 +38,65 @@ namespace(this, "automata.games.openTheGate", function (exports) {
         onReset: function () {
             this.gateY = this.gateYMax;
             this.carX = [this.carXMin, this.carXMin];
+            this.carMoving = [true, false];
             
             this.upAndDownAtTheSameTime = false;
             this.crush = false;
         },
         
         onStep: function () {
-            // Update car position. The car can move:
-            // * before reaching the "Open" button (carXStop),
-            // * through the the gate, if the gate is open,
-            // * after the gate.
-            if(this.carX[0] < this.carXStop ||
-               (this.carX[0] >= this.carXStop && this.carX[0] < this.gateX + this.gateWidth && this.gateY < this.gateYOpen) ||
-               (this.carX[0] >= this.gateX + this.gateWidth && this.carX[0] < this.carXMax))
-                this.carX[0] += this.carXStep;
-            
             // Update gate position according to open and close commands.
             // * The gate moves up when the Open command is on
-            // * The gate moves down when the Close command is on and no car is passing
+            // * The gate moves down when the Close command is on and no door has been crushed
             if(this.getActuatorValue(0) === "1" && this.getActuatorValue(1) === "0" && this.gateY > this.gateYMin) {
                 this.gateY -= this.gateYStep;
             }
-            if(this.getActuatorValue(0) === "0" && this.getActuatorValue(1) === "1" && this.gateY < this.gateYMax &&
-               (this.carX[0] < this.gateX - this.carWidth || this.carX[0] > this.gateX + this.gateWidth || this.gateY < this.gateYOpen)) {
+            if(this.getActuatorValue(0) === "0" && this.getActuatorValue(1) === "1" && this.gateY < this.gateYMax && !this.crush) {
                 this.gateY += this.gateYStep;
             }
             
-            // Push the button when the car is in front of the gate until the gate starts to open
-            if(this.carX[0] >= this.carXStop && this.carX[0] <= this.carXStop + this.carXStep && this.gateY >= this.gateYMax)
-                this.setSensorValue(0, "1");
-            else
-                this.setSensorValue(0, "0");
-            
-            // Update gate status signals
-            this.setSensorValue(1, (this.gateY <= this.gateYMin)? "1" : "0");
-            this.setSensorValue(2, (this.gateY >= this.gateYMax)? "1" : "0");
-            
-            // Update vehicle sensor
-            this.setSensorValue(3, (this.carX[0] + this.carWidth >= this.vehicleSensorXMin && this.carX[0] <= this.vehicleSensorXMax)? "1" : "0");
-
             // Update problem indicators.
             // The will stay true until the end.
             if (this.getActuatorValue(0) === "1" && this.getActuatorValue(1)  === "1") {
                 this.upAndDownAtTheSameTime = true;
             }
+
+            // Update gate status signals
+            this.setSensorValue(1, (this.gateY <= this.gateYMin)? "1" : "0");
+            this.setSensorValue(2, (this.gateY >= this.gateYMax)? "1" : "0");
+
+            // Reset button and vehicle sensor
+            this.setSensorValue(0, "0");
+            this.setSensorValue(3, "0");
             
-            if (this.getActuatorValue(1) === "1" && this.carX[0] > this.gateX - this.carWidth && this.carX[0] < this.gateX + this.gateWidth) {
-                this.crush = true;
+            forEach (x, index of this.carX) {
+                if (this.carMoving[index]) {
+                    // Update car position. A car can move:
+                    // * before reaching the "Open" button (carXStop),
+                    // * through the the gate, if the gate is open,
+                    // * after the gate.
+                    if(x < this.carXStop ||
+                       (x >= this.carXStop && x < this.gateX + this.gateWidth && this.gateY < this.gateYOpen) ||
+                       (x >= this.gateX + this.gateWidth && x < this.carXMax))
+                        x += this.carXStep;
+                    
+                    this.carX[index] = x;
+                    
+                    // Push the button when the car is in front of the gate until the gate starts to open
+                    if(x >= this.carXStop && x <= this.carXStop + this.carXStep && this.gateY >= this.gateYMax) {
+                        this.setSensorValue(0, "1");
+                    }
+                    
+                    // Detect car crushing under gate
+                    if(x >= this.gateX - this.carWidth && x <= this.gateX + this.gateWidth && this.gateY > this.gateYOpen) {
+                        this.crush = true;
+                    }                    
+
+                    // Update vehicle sensor
+                    if (x + this.carWidth >= this.vehicleSensorXMin && x <= this.vehicleSensorXMax) {
+                        this.setSensorValue(3, "1");
+                    }
+                }
             }
         },
         
